f4daf1e882827e5d80600a88630605afae53734f
KarmaPolice2802 <sv46793719@idat.pe>
2025-02-20 08:36:33 -0500

    nuevas clases

---
+++ src/main/java/pe/edu/idat/demo_apirest/controller/FundamentosController.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.demo_apirest.controller;
+
+public class FundamentosController {
+}
---
+++ src/main/java/pe/edu/idat/demo_apirest/service/iFundamentosService.java
@@ -0,0 +1,4 @@
+package pe.edu.idat.demo_apirest.service;
+
+public interface iFundamentosService {
+}
