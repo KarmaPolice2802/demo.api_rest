d0a34b4add41507bd94fc916310cde144f4deb79
KarmaPolice2802 <sv46793719@idat.pe>
2025-02-20 08:06:36 -0500

    initial commit

---
+++ src/test/java/pe/edu/idat/demo_apirest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.demo_apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
