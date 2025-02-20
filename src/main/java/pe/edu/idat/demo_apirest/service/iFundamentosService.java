f32263a9e252f55669d20adc18d19ae2fed9aac7
KarmaPolice2802 <sv46793719@idat.pe>
2025-02-20 10:03:16 -0500

    algoritmos del servicio finalizado

--- src/main/java/pe/edu/idat/demo_apirest/service/iFundamentosService.java
+++ src/main/java/pe/edu/idat/demo_apirest/service/iFundamentosService.java
@@ -1,4 +1,12 @@
 package pe.edu.idat.demo_apirest.service;
 
+import java.util.List;
+
 public interface iFundamentosService {
+
+    String validarNumeroPrimo(int numero);
+
+    String calcularFactorial(int numero);
+
+    List<Integer> calcularFibonacci(int numero);
 }
